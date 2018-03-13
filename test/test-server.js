c209f3deaa67204a07c7ddc160c5f3ebeaaec49a
thabtom <tsehayegh@gmail.com>
2018-03-12 20:50:32 -0400

    added a testing to get method to the dashboard

--- test/test-server.js
+++ test/test-server.js
@@ -4,6 +4,7 @@ const mocha = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const should = chai.should();
 
 const app = require('../server.js');
 
@@ -12,10 +13,19 @@ chai.use(chaiHttp);
 describe('index page', function(){
 	it('should check if page exists', function(){
 		return chai.request(app)
-			.get('/')
+			.get('/dashboard')
 		 	.then(function(res){
 				expect(res).to.have.status(200);
 			});
 	});
+
+	it('should load the map', function(){
+		return chai.request(app)
+		.get('/dashboard')
+		.then(function(res){
+			res.should.have.status(200);
+			res.body.should.be.a('object');
+		});
+	})
 });
 
