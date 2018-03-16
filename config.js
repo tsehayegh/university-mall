f6860ea02ada50b2d5802c88c353ffbab4b5b22f
thabtom <tsehayegh@gmail.com>
2018-03-16 00:12:55 -0400

    added white space in config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/university-mall';
+
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-university-mall';
 
 exports.PORT = process.env.PORT || 8080;
